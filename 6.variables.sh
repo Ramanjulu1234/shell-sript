2098997f67b0dd151f17a29335e844da8cbd8c02
Ramanjulu1234 <ramanjulu7890@gmail.com>
2024-04-18 21:56:32 +0530

    USERNAME

--- 6.variables.sh
+++ 6.variables.sh
@@ -2,10 +2,10 @@
 
 echo "please enter user name::"
 
-read USERNAME
+read -s USERNAME
 
 echo "please enter password::"
 
-read PASSWORD
+read -s PASSWORD
 
 echo "username is: $USERNAME, password is : $PASSWORD"
