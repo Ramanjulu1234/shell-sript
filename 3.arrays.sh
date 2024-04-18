f2f3d621d6077bd4a8216ac6c4962540fc066d95
Ramanjulu1234 <ramanjulu7890@gmail.com>
2024-04-18 22:05:12 +0530

    arrays

--- 3.arrays.sh
+++ 3.arrays.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
-
 MOVIES=("okkadu" "athadu" "murari")
+
+#size of above array is 3.
+#index are 0,1,2.
+#list always starts with 0.
+
 echo "first movie is : ${MOVIES[0]}"
 echo "second movie is : ${MOVIES[1]}"
 echo "All movies are : ${MOVIES[@]}"
