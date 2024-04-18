4c3deeec64211a5a664a94e1531934326f5f4302
Ramanjulu1234 <ramanjulu7890@gmail.com>
2024-04-18 12:02:37 +0530

    variable1

---
+++ 1.variables.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+
+echo "Ramesh:: Hello suresh. how are you?"
+echo "suresh:: Hii ramesh. Iam fine...How are you?"
+echo "Ramesh:: I am fine too. How is your work?"
+echo "suresh:: not bad. I am thunking to upgrade to devops."
