a4a7152191f779361bb5adc3a99eeb15c8844b2a
Ramanjulu1234 <ramanjulu7890@gmail.com>
2024-04-18 12:21:25 +0530

    variables1

--- 1.variables.sh
+++ 1.variables.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
+PERSON1=Ramesh
+PERSON2=suresh
 
-
-echo "Ramesh:: Hello suresh. how are you?"
-echo "suresh:: Hii ramesh. Iam fine...How are you?"
-echo "Ramesh:: I am fine too. How is your work?"
-echo "suresh:: not bad. I am thunking to upgrade to devops."
+echo "$PERSON1:: Hello $PERSON2. how are you?"
+echo "$PERSON2:: Hii $PERSON1. Iam fine...How are you?"
+echo "$PERSON1:: I am fine too. How is your work?"
+echo "$PERSON2:: not bad. I am thunking to upgrade to devops."
